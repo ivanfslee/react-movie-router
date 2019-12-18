0183dd68bd2892a6ba8e5d8c1c5d6b0c5d910e22
ivanfslee <ivanfslee@gmail.com>
2019-12-18 08:38:46 -0800

    Removed comments

--- src/Home.js
+++ src/Home.js
@@ -42,8 +42,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-// const apiKey = "fec8b5ab27b292a68294261bb21b04a5";
-// const singleMovieUrl = "https://api.themoviedb.org/3/movie/"
-// const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing";
-// const imageUrl = "http://image.tmdb.org/t/p/w300";
